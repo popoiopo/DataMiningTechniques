47b339b6270b3a7739fb915186bb1b879c4e33ca
popoiopo <bastiaan.chatel@gmail.com>
2018-04-19 16:46:15 +0200

    vorige keer

--- Assignment1/Python/init.py
+++ Assignment1/Python/init.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import numpy as np
+from bokeh.io import show, output_file
+from bokeh.models import ColumnDataSource
+import randomcolor
+import math
+from bokeh.models import HoverTool, LabelSet, SaveTool
+from bokeh.plotting import figure
