02819a888f9800eae802c2cd8b49aa8689841942
Julia <juliaanten@hotmail.com>
2018-04-22 19:12:51 +0200

    changed the score function

--- Assignment1/Python/cross_validation.py
+++ Assignment1/Python/cross_validation.py
@@ -36,6 +36,8 @@ good.pop(0)
 data = data[good,:]
 target = target[good]
 
+
+
 known = []
 
 # Only three unknowns, not usefull for cross-validation
@@ -56,8 +58,8 @@ target=target.astype('int')
 
 # Setup the models
 
-clf_linear = svm.SVC(kernel='linear', C=10)
-clf_poly = svm.SVC(kernel='poly', C=10)
+clf_linear = svm.SVC(kernel='linear', C=1)
+clf_poly = svm.SVC(kernel='poly', C=1)
 
 
 
@@ -68,27 +70,30 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 clf_linear.fit(X_train, y_train)	
 predictions_linear = clf_linear.predict(X_test)
+scores_linear = clf_linear.score(X_test,y_test)
+
 
 clf_poly.fit(X_train, y_train)
 predictions_poly = clf_poly.predict(X_test)
+scores_poly = clf_poly.score(X_test, y_test)
 
-accuracy_linear = metrics.accuracy_score(y_test, predictions_linear)
-accuracy_poly = metrics.accuracy_score(y_test, predictions_poly)
 
-print("Test/Train:\nlinear: ", accuracy_linear, "polynomial: ", accuracy_poly)
+print("Test/Train\n")
+
+print("Accuracy linear: %0.3f (+/- %0.3f)" % (scores_linear.mean(), scores_linear.std() * 2))
+print("Accuracy polynomial: %0.3f (+/- %0.3f)\n" % (scores_poly.mean(), scores_poly.std() * 2))
 
 
 
 # Cross validation
 
 pred_cross_linear = cross_val_predict(clf_linear, data, target, cv=5)
-pred_cross_poly = cross_val_predict(clf_linear, data, target, cv=5)
+pred_cross_poly = cross_val_predict(clf_poly, data, target, cv=5)
 
-accuracy_linear_cross = metrics.accuracy_score(target, pred_cross_linear)
-accuracy_poly_cross = metrics.accuracy_score(target, pred_cross_poly)
-
-print("Cross Validation\nlinear: ", accuracy_linear_cross, "polynomial: ", accuracy_poly_cross)
+scores_cross_linear = cross_val_score(clf_linear, data, target, cv=5)
+scores_cross_poly = cross_val_score(clf_poly, data, target, cv=5)
 
 
-
-
+print("Cross Validation\n")
+print("Accuracy linear: %0.3f (+/- %0.3f)" % (scores_cross_linear.mean(), scores_cross_linear.std() * 2))
+print("Accuracy polynomial: %0.3f (+/- %0.3f)\n" % (scores_cross_poly.mean(), scores_cross_poly.std() * 2))
